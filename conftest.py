5b366ea5db64ade3ad36f2ee01d827e445f1ddf4
echpoch2 <61111925+echpoch2@users.noreply.github.com>
2020-11-30 22:10:47 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,6 +3,6 @@ from selenium.webdriver import Opera
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = Opera('D:\web\operadriver.exe')
+    driver = Opera(executable_path='D:\web\operadriver.exe')
     yield driver
     driver.quit()
