fbc08847841618eeb4116eb2ef2d8385655c5c2a
Ilia Naibich <nice.naybich@gmail.com>
2020-12-30 09:04:05 +0300

    xpath fix

--- pages/locatorsMainPage.py
+++ pages/locatorsMainPage.py
@@ -6,7 +6,7 @@ class MainPageLocators():
     EMAIL_INPUT = (By.CLASS_NAME, 'bx-form-control')
     SUBSCRIBE_BUTTON = (By.ID,'bx_subscribe_btn_sljzMT')
     POPUP_HEADER = (By.XPATH,'//*[@id="sender-subscribe-response-cont"]/div/div[1]')
-    SEARCH_INPUT = (By.XPATH,'/html/body/div[5]/section[2]/div/div[1]/div/form/input[1]')
+    SEARCH_INPUT = (By.XPATH,'/html/body/div[3]/section[2]/div/div[1]/div/form/input[1]')
     SEARCH_BUTTON = (By.CLASS_NAME,"button-search")
     ERROR_LABEL = (By.CLASS_NAME, "errortext")
     NOTE_LABEL = (By.CLASS_NAME,"notetext")
